8f07d194666cdb8bfbe8ef0b358a7eaee7c61b89
Rahul Reekhadi <rahulreekhadi87@gmail.com>
2026-01-13 17:26:54 +0530

    Fixing cors error

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ def create_app():
     load_config(app)
     CORS(
         app,
-        resources={r"/api/*": {"origins": "https://stt-to-tts.vercel.app/"}},
+        resources={r"/api/*": {"origins": "https://stt-to-66bzy7qy8-rahulreekhadi1s-projects.vercel.app/"}},
         supports_credentials=True,    
         methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],     
         allow_headers=["Content-Type", "Authorization"]
